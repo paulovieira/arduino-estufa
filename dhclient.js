8634fb9ba856f8e62469f93ada53f327df6ffc65
Paulo Vieira <paulovieira@gmail.com>
2016-02-18 16:38:44 +0000

    update

--- dhclient.js
+++ dhclient.js
@@ -8,14 +8,17 @@ var errorsPath = Path.join(__dirname, "./data/errors.log");
 
 Dns.lookup("google.com", {family: 4}, function(err, address, family) {
 
-	if(err){
+    if(err){
         err["ts"] = new Date().toISOString();
         console.log(JSON.stringify(err, ["message", "code", "errno", "syscall", "ts"], 4));
 
         Fs.appendFile(errorsPath, JSON.stringify(err, ["message", "code", "errno", "syscall", "ts"], 4) + "\n\n");
         Db.query(`INSERT INTO errors(data) VALUES('${ JSON.stringify(err, ["message", "code", "errno", "syscall", "ts"]) }');`)
 
-		Shell.exec("/sbin/dhclient -r");
-	}
+        var output = Shell.exec("sudo /sbin/dhclient -r -v");
+	output["ts"] = new Date().toISOString();
+        Fs.appendFile(errorsPath, JSON.stringify(output, null, 4) + "\n\n");
+	
+    }
 
 });
