57daa3d9bacd5da5f53635cc564e5e9816fe272b
Paulo Vieira <paulovieira@gmail.com>
2016-06-16 16:05:09 +0100

    add sudo to the restart script

--- arduino_read_restart.sh
+++ arduino_read_restart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ps -aux | gawk '$11 == "arduino_read" { print $2}' | xargs kill --signal SIGINT;
+ps -aux | gawk '$11 == "arduino_read" { print $2}' | sudo xargs kill --signal SIGINT;
 sleep 1s;
 
 # cwd should be the adjusted before the script is executed
